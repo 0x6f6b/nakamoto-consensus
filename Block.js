8584a6899a25dfbca1865b875c2a2deed76049da
0x6f6b <jackmertensdev@gmail.com>
2022-05-28 22:37:40 +0100

    Proof-of-Work mechanism implemented and working

--- Block.js
+++ Block.js
@@ -1,4 +1,6 @@
-CryptoJS = require('crypto-js');
+const CryptoJS = require('crypto-js');
+
+const TARGET = 0xFAB4643C9EECCE1AA639D1A4284AE63DCDDF3479F584E2135AFF04D6A76n; // about 20 seconds on my cpu
 
 class Block {
   constructor(transactions, previousHash) {
@@ -6,25 +8,58 @@ class Block {
     this.previousHash = previousHash;
   }
 
-  calculateProofOfWork(difficulty) {
+  calculateProofOfWork(target) {
+    console.log("Calculating proof of work...");
     this.nonce = 0;
     let hash = this.calculateHash();
-    while (hash >= difficulty) {
-      this.nonce++;
+
+    // convert hash to a big number
+    let hashValue = BigInt("0x" + hash);
+
+    while (hashValue >= target) {
+      this.nonce = Math.random().toString();
       hash = this.calculateHash();
+      hashValue = BigInt("0x" + hash);
     }
+
     console.log(`Found proof of work: ${hash}, nonce: ${this.nonce}`);
+    this.hash = hash;
+    this.timestamp = Date.now();
   }
 
   calculateHash() {
-    return CryptoJS.SHA256(this.previousHash + this.transactions + this.nonce).toString();
+    return CryptoJS.SHA256(this.blockNumber + this.previousHash + this.transactions + this.nonce).toString();
   }
 }
 
-function genesisBlock() {
-  return new Block(
-    [coinbaseTransaction('0')],
-    '0'
-  );
+function genesisBlock(db) {
+  console.log("Creating the Genesis Block...");
+  // creating the genesis block
+  genesisBlock = new Block(null, "0");
+
+  // calculate the proof of work
+  console.log("Starting the proof of work...");
+  genesisBlock.calculateProofOfWork(TARGET);
+  console.log("finished the proof of work...");
+
+  // set the lastHash to the genesis block's hash
+  db.put("lastHash", genesisBlock.hash);
+  console.log("last hash set to genesis block's hash");
+
+  // set the lastBlockNumber to 0
+  db.put("lastBlockNumber", 0);
+  console.log("last block number set to 0");
+
+  db.put(genesisBlock.hash, genesisBlock); // add the genesis block to the blockchain"
+  console.log("added the genesis block to the blockchain");
 }
 
+async function addBlock(blockchain, newBlock) {
+  newBlock.calculateProofOfWork(TARGET)
+
+  await blockchain.put("lastHash", newBlock.calculateHash());
+  await blockchain.put("lastBlockNumber", newBlock.blockNumber);
+  await blockchain.put(newBlock.calculateHash(), newBlock);
+}
+
+module.exports = { addBlock, genesisBlock, Block: Block };
--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const { Level } = require('level')
+const { Level } = require('level');
+const fs = require('fs');
 
-main()
-async function main() {
-  // Create a database
-  const db = new Level(__dirname + '/blocks', { valueEncoding: 'json' })
+const BlockUtils = require('./Block');
+const Block = BlockUtils.Block;
 
-  const value = await db.get('block1')
+// Create a database / access an existing one
+const db = new Level(__dirname + '/blockchain', { valueEncoding: 'json' })
 
-  console.log("value: ", value)
+if (!fs.existsSync(__dirname + '/blockchain')) {
+  // the blockchain does not exist, so create it
+  intialiseBlockchain()
+  console.log("Restart the application to mine new blocks")
+} else {
+  mine()
+}
+
+function intialiseBlockchain() {
+  db.put("lastHash", "0");
+  // generate the genesis block
+  BlockUtils.genesisBlock(db);
+  console.log("The Genesis Block has been created")
+}
+
+async function mine() {
+  // get the hash of the last block
+  const lastHash = await db.get("lastHash");
+
+  // get the last block number
+  const lastBlockNumber = await db.get("lastBlockNumber");
+
+  // fetch the transactions from the mempool
+  const transactions = getTransactions();
+
+  // create a new block
+  const newBlock = new Block(transactions, lastHash)
+  newBlock.blockNumber = lastBlockNumber + 1;
+
+  // add the block to the blockchain and persist it
+  await BlockUtils.addBlock(db, newBlock);
+
+  // recursively call this function to mine the next block
+  mine();
+}
+
+function getTransactions() {
+  return null
 }
