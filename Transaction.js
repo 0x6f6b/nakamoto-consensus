a8f4814a1c5c17c5535d77df42e8b3a15389ed97
0x6f6b <94190346+0x6f6b@users.noreply.github.com>
2022-06-05 21:11:41 +0000

    Started transactions

--- Transaction.js
+++ Transaction.js
@@ -1,10 +1,28 @@
 const COINBASE_AMOUNT = 50;
 
-class Transaction {
-  constructor(fromAddress, toAddress, amount) {
-    this.fromAddress = fromAddress;
-    this.toAddress = toAddress;
+class TxInput { // a reference to an output from another transaction
+  constructor(txId, txOutIndex, signature) {
+    this.txId = txId;
+    this.txOutIndex = txOutIndex;
+    this.signature = signature;
+  }
+}
+
+class TxOutput {
+  constructor(address, amount) {
+    this.address = address;
     this.amount = amount;
+  }
+}
+
+class Transaction {
+  constructor(fromAddress, toAddress, amount, db) {
+    // firstly, create the inputs for the transaction
+    this.txIns = [];
+
+    // iterate through the chain until we find valid outputs to spend
+    let total = 0;
+    
   }
 }
 
